3aad538072643645b07cda3c2a6bf36eaa16741a
PeterL93 <Peter_sl5@hotmail.com>
2017-10-15 20:31:55 +0200

    Add files via upload

---
+++ day2exercise6/exercise6/src/MyFile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import image from './public/image';
+
+export default class MyFile extends Component{
+    
+
+
+    render(){
+        return (
+            <p>Hello this is my new file</p>
+        );
+
+    }
+}
